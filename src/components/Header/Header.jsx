3417ee530f7361f841e4d68109faa30c900af9dd
melochik <ikuchin2005@gmail.com>
2022-08-19 19:35:41 +0200

    header icon - link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import head from "./Header.module.css"
 import React from "react"
+import { NavLink } from "react-router-dom"
 const Header = () => {
     return (
         <header className={head.header}>
-            <img className={head.logo} src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png"></img>
+            <NavLink to="/"><img className={head.logo} src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" /></NavLink>
         </header>
     )
 }
