7a0bad0f98803421133fbb158311f3c412d1bda9
melochik <ikuchin2005@gmail.com>
2022-09-20 19:50:32 +0200

    add styles to user

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import d from "./Users.module.css"
 
 export let Users = (props) => {
-    let users = props.users.map(u => <div key={u.id}>
-        <span>
+    let users = props.users.map(u => <div key={u.id} className={d.hey}>
+        <span className={d.userImgBtn}>
             <div><img src={u.img} width="100" /></div>
             <div>{u.followed ?
                 <button onClick={() => { props.unfollow(u.id) }}>UnFollow</button> :
                 <button onClick={() => { props.follow(u.id) }}>Follow</button>}</div>
         </span>
 
-        <span>
+        <span className={d.userInfo}>
             <span>
                 <div>{u.name}</div>
                 <div>{u.describe}</div>
             </span>
-            <span>
+            <span className={d.location}>
                 <div>{u.location.country}</div>
                 <div>{u.location.city}</div>
             </span>
